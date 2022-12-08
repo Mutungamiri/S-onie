5e9f4f852d0586d38a52425d25d6b1f713f03c43
Mateusz Perdek <49455873+Mutungamiri@users.noreply.github.com>
2022-12-08 23:47:09 +0100

    Add files via upload

--- slonie/main.py
+++ slonie/main.py
@@ -1,46 +1,29 @@
 """ 
 Zadanie: Słonie
-Sposób uruchomienia: np. python main.py slo1.in 
+Sposób uruchomienia: python main.py < plik.in
 Autor: Mateusz Perdek
 """
-import sys
-
-#funkcja służąca do wczytywania kolejnych lini z pliku
-def wczytaj(rozmiar, plik):
-    temp = []
-    linia = plik.readline().split()
-    for i in range(rozmiar):
-        temp.append(int(linia[i]))
-    return temp
-
 def main():
-    #wczytanie pliku
-    nazwa_pliku = sys.argv[1]
-    plik = open(nazwa_pliku, 'r')
-    linia = plik.readline().split()
-    liczba_sloni = int(linia[0]) #wczytanie liczby słoni (rozmiar zadania)
-
-    #wczytanie wag
-    wagi = wczytaj(liczba_sloni, plik) 
-    permutacja_pierwotna = []
-    permutacja_docelowa = [0] * liczba_sloni
+    
+    # wczytanie liczby słoni 
+    liczba_sloni = int(input())
+    
+    # wczytanie wag
+    temp = input().split()
+    wagi = [int(waga) for waga in temp]
     minimalna_waga = min(wagi)
 
-    #wczytanie permutacji początkowej
-    temp = []
-    linia = plik.readline().split()
-    for i in range(liczba_sloni):
-        temp.append(int(linia[i]))
-        temp[i] = temp[i] - 1
-    permutacja_pierwotna = temp
+    # wczytanie permutacji początkowej
+    temp = input().split()
+    permutacja_pierwotna = [int(pier) - 1 for pier in temp]
 
-    #przypisanie miejsc docelowych
-    numer = 0
-    linia = plik.readline().split()
+    # przypisanie miejsc docelowych
+    temp = input().split()
+    permutacja_temp = [int(tem) - 1 for tem in temp]
+    permutacja_docelowa = [0] * liczba_sloni
     for i in range(liczba_sloni):
-        numer = int(linia[i])
-        numer = numer - 1
-        permutacja_docelowa[numer] = permutacja_pierwotna[i]
+         numer = permutacja_temp[i]
+         permutacja_docelowa[numer] = permutacja_pierwotna[i]
 
 
     zbadany_cykl = [False] * liczba_sloni
@@ -64,7 +47,6 @@ def main():
             wynik += min(suma+(dlugosc_cyklu-2)*minimalna_waga_cykl, suma+minimalna_waga_cykl+(dlugosc_cyklu+1)*minimalna_waga)
 
     print(wynik)
-    plik.close()
 
 if __name__ == "__main__":
     main()
